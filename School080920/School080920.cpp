f1224a6b8732e6b862e2eb193da46a51aa4e7d23
roseheart13 <70570030+roseheart13@users.noreply.github.com>
2020-09-08 15:06:22 +0200

    Loop

--- School080920/School080920.cpp
+++ School080920/School080920.cpp
@@ -5,16 +5,18 @@
 
 int main()
 {
+    bool flag = true; //another way to stopp it
     char option = 0;
 
-    while (true) {
+    //while (option != 'Q' && option != 'q') one way to stop loop
+    while (flag == true) {
         std::cout << "A. Save the game" << std::endl;
         std::cout << "B. Restart teh game" << std::endl;
         std::cout << "C. Select difficulty" << std::endl;
         std::cout << "  Select an option: ";
         std::cin >> option;
 
-        if (option == 'A' or option == 'a') {
+        /*if (option == 'A' or option == 'a') {
             std::cout << "Saving ..." << std::endl;
         }
         else if (option == 'B' or option == 'b') {
@@ -25,7 +27,28 @@ int main()
         }
         else {
             std::cout << " Invalid input ..." << std::endl;
+        }*/
+
+        //easy to check with switch
+        switch (option) {
+        case 'A':case 'a':
+            std::cout << " Saving ..." << std::endl;
+                break;
+        case 'B':case 'b':
+            std::cout << " restarting the game ..." << std::endl;
+            break;
+        case 'C':case 'c':
+            std::cout << " Please select difficulty (easy/hard) ..." << std::endl;
+            break;
+        case 'Q':case 'q':
+            flag = false;
+
+        default:
+            std::cout << " Invalid input ..." << std::endl;
+            break;
         }
+        
+
     }
  }
 
